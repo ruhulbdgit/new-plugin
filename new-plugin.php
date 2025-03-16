b70d2d65613ffa90bcae128cfd65c513facce696
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2025-03-16 22:05:09 +0600

    header requirement for Plugin

--- new-plugin.php
+++ new-plugin.php
@@ -1,6 +1,18 @@
 <?php
 
 /*
-Plugin Name: New Plugin
-Author: Ruhul siddiki
-*/
+ * Plugin Name:       New Plugin
+ * Plugin URI:       #
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ruhul Siddiki
+ * Author URI:       #
+ * License:           GPL v2 or later
+ * License URI:       #
+ * Update URI:        #
+ * Text Domain:       new-basics-plugin
+ * Domain Path:       /languages
+ * Requires Plugins:  #
+ */
