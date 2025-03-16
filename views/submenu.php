2d8a7271744a0e8bebe084b9dd8e34f2fe3fcd31
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2025-03-17 04:16:28 +0600

    update

--- views/submenu.php
+++ views/submenu.php
@@ -1,2 +1,17 @@
-<?php
-echo '<h2>Main menu Page</h2>';
+<?php 
+echo plugin_dir_path( __FILE__ );
+echo "<br/>";
+echo PLUGIN_BASENAME;
+echo "<h2 class='sub'>Submenu</h2>";
+echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod saepe, 
+laborum temporibus tempore laboriosam laudantium reprehenderit rem
+ voluptate nemo blanditiis architecto, autem possimus harum provident
+  minus asperiores beatae sint repellat.</p>";
+
+  echo "<br/>";
+  echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod saepe, 
+laborum temporibus tempore laboriosam laudantium reprehenderit rem
+ voluptate nemo blanditiis architecto, autem possimus harum provident
+  minus asperiores beatae sint repellat.</p>";
+
+  echo "<br/>";
